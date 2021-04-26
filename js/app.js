a6803fbdf87db2b8402a7cc138609a5c8e807b12
maryna1408 <cabanmarina9@gmail.com>
2021-04-26 14:55:59 +0300

    add confirmedSum

--- js/app.js
+++ js/app.js
@@ -1,16 +1,21 @@
 const rowListEl = document.getElementById('rowList')
-
+const globalNumConfirmedEl = document.getElementById('globalNumConfirmed')
 
 
 
 
 let DATA = []
+let confirmed = []
 
 async function getData(url) {
     const response = await fetch(url)
     console.log(response)
     DATA = await response.json()
     renderRows(DATA.world, rowListEl)
+    DATA.world.forEach( el => {
+        confirmed.push(el.confirmed)
+    }) 
+    globalNumConfirmedEl.textContent = confirmed.reduce((total, amount) => total + amount)
     console.log(DATA)
 }
 
@@ -22,16 +27,21 @@ function renderRows(data_array, node) {
     let html = ''
   
     data_array.forEach(el => html += createRowHTML(el));
+
   
     node.innerHTML = html
   }
   
 function createRowHTML(card_data) {
     return `<div class="cards d-flex justify-content-between p-3 fs-4">
-    <div class="country">${card_data.country}</div>
-    <div class="confirmed">${card_data.confirmed}</div>
-    <div class="deaths">${card_data.deaths}</div>
-    <div class="recovered">${card_data.recovered}</div>
-    <div class="existing">${card_data.existing}</div>
+    <div class="country w-100">${card_data.country}</div>
+    <div class="confirmed w-100">${card_data.confirmed}</div>
+    <div class="deaths w-100">${card_data.deaths}</div>
+    <div class="recovered w-100">${card_data.recovered}</div>
+    <div class="existing w-100">${card_data.existing}</div>
 </div>`
 }
+
+
+
+
