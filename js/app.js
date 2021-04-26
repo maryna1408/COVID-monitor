357489e7fc872ecdacc694cf36313f7fd35c4c44
maryna1408 <cabanmarina9@gmail.com>
2021-04-26 16:09:31 +0300

    add reduce to header

--- js/app.js
+++ js/app.js
@@ -1,21 +1,32 @@
 const rowListEl = document.getElementById('rowList')
 const globalNumConfirmedEl = document.getElementById('globalNumConfirmed')
-
+const globalNumDeathsEl = document.getElementById('globalNumDeaths')
+const globalNumRecoveredEl = document.getElementById('globalNumRecovered')
+const globalNumExistingEl = document.getElementById('globalNumExisting')
 
 
 
 let DATA = []
-let confirmed = []
 
 async function getData(url) {
     const response = await fetch(url)
     console.log(response)
     DATA = await response.json()
     renderRows(DATA.world, rowListEl)
+    let confirmed = []
+    let deaths = []
+    let recovered = []
+    let existing = []
     DATA.world.forEach( el => {
-        confirmed.push(el.confirmed)
+        confirmed.push(el.confirmed), 
+        deaths.push(el.deaths), 
+        recovered.push(el.recovered), 
+        existing.push(el.existing)
     }) 
     globalNumConfirmedEl.textContent = confirmed.reduce((total, amount) => total + amount)
+    globalNumDeathsEl.textContent = deaths.reduce((total, amount) => total + amount)
+    globalNumRecoveredEl.textContent = recovered.reduce((total, amount) => total + amount)
+    globalNumExistingEl.textContent = existing.reduce((total, amount) => total + amount)
     console.log(DATA)
 }
 
@@ -33,7 +44,7 @@ function renderRows(data_array, node) {
   }
   
 function createRowHTML(card_data) {
-    return `<div class="cards d-flex justify-content-between p-3 fs-4">
+    return `<div class="cards d-flex justify-content-between  p-3 fs-4">
     <div class="country w-100">${card_data.country}</div>
     <div class="confirmed w-100">${card_data.confirmed}</div>
     <div class="deaths w-100">${card_data.deaths}</div>
