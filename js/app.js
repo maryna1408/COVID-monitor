f5d9a35dfb9301657e7e8db5298ee85c8d96d91e
maryna1408 <cabanmarina9@gmail.com>
2021-04-26 13:39:14 +0300

    fetch fixes

--- js/app.js
+++ js/app.js
@@ -10,11 +10,11 @@ async function getData(url) {
     const response = await fetch(url)
     console.log(response)
     DATA = await response.json()
-    renderCards(DATA, cardListEl)
+    renderCards(DATA.world, cardListEl)
     console.log(DATA)
 }
 
-getData('https://api-covid19.rnbo.gov.ua/data?to=2021-04-19')
+getData('https://api-covid19.rnbo.gov.ua/data?to=2021-04-24')
 
 
 function renderCards(data_array, node) {
@@ -26,11 +26,11 @@ function renderCards(data_array, node) {
   }
   
 function createCardHTML(card_data) {
-    return `<div class="card h-25">
-    <div class="country"></div>
-    <div class="confirmed"></div>
-    <div class="deaths"></div>
-    <div class="recovered"></div>
-    <div class="existing"></div>
+    return `<div class="cards d-flex justify-content-between p-3 fs-4">
+    <div class="country">${card_data.country}</div>
+    <div class="confirmed">${card_data.confirmed}</div>
+    <div class="deaths">${card_data.deaths}</div>
+    <div class="recovered">${card_data.recovered}</div>
+    <div class="existing">${card_data.existing}</div>
 </div>`
 }
